343453641927cda7aaf5c877882edd176fc5cfef
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-11 01:33:54 -0300

    Update backup-banco.sh

--- backup-banco.sh
+++ backup-banco.sh
@@ -1,4 +1,4 @@
 DATA=$(date +'%d-%m-%Y')
 backp= $HOME/backups-database
 mkdir -p $HOME/backups-databases|| exit 1
-mysqldump --all-databases > $backps/backup-$DATA.sql
+mysqldump --all-databases > $backp/backup-$DATA.sql
