a93bc456b7f86b7fd1d90a0be0b116ec3b0d57f8
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-11 01:19:45 -0300

    Update backup-banco.sh

--- backup-banco.sh
+++ backup-banco.sh
@@ -1,4 +1,4 @@
-diabackup= $(date "+%Y%m%d")
-backp= $HOME/backups-databases/backup-$diabackup.sql
+dia= $(date "+%Y%m%d")
+backp= $HOME/backups-databases/backup-$dia.sql
 mkdir -p $HOME/backups-databases|| exit 1
 mysqldump --all-databases -u root > $backp
