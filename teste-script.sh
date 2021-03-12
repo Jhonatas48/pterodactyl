482a748cc37c6bc6a82a3d06b08c1e6a612390d9
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-12 11:45:39 -0300

    Update

--- teste-script.sh
+++ teste-script.sh
@@ -7,5 +7,6 @@ DRT=/backups-databases/backup-$DATA.sql
 if ["$SENHA" = "$VAZIO"];then
    mysqldump -u USR --all-databases > $DRT
 else
+   acho $SENHA
    mysqldump -u $USR --password=$SENHA --all-databases > $DRT
 fi
