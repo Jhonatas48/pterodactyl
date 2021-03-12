e2ddb8ef4ee68dde4b833ef4d53d770173a78398
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-12 01:56:33 -0300

    Alteração do script para adição de var para usuari

--- backup-banco.sh
+++ backup-banco.sh
@@ -1,6 +1,6 @@
 USR=root
-SENHA=""
+SENHA=''
 DATA=$(date +'%d-%m-%Y')
 mkdir -p /backups-databases || exit 1
 DRT=/backups-databases/backup-$DATA.sql
-mysqldump -u $USR -p $SENHA --all-databases > $DRT
+mysqldump -u USR --password=$SENHA > $DRT
