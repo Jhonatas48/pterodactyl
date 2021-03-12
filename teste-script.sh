493a7c498e174c83653d8b39ccb305cf3fae7beb
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-12 02:54:20 -0300

    Create teste-script.sh

---
+++ teste-script.sh
@@ -0,0 +1,11 @@
+USR=teste
+SENHA='testando'
+DATA=$(date +'%d-%m-%Y')
+mkdir -p /backups-databases || exit 1
+DRT=/backups-databases/backup-$DATA.sql
+if [-z "$SENHA"]
+then
+   mysqldump -u USR --all-databases > $DRT
+else
+   mysqldump -u $USR --password=$SENHA --all-databases > $DRT
+fi
