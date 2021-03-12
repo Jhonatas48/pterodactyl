687fc28fed29c753b4de8994fe9b3e379a6beec9
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-12 01:58:42 -0300

    Update do comando

--- backup-banco.sh
+++ backup-banco.sh
@@ -1,6 +1,6 @@
 USR=root
-SENHA=""
+SENHA=''
 DATA=$(date +'%d-%m-%Y')
 mkdir -p /backups-databases || exit 1
 DRT=/backups-databases/backup-$DATA.sql
-mysqldump -u USR --password=$SENHA > $DRT
+mysqldump -u USR --password=$SENHA --all-databases > $DRT
