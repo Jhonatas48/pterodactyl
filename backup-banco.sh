fc2f3831f197e04530d2c3fe9ec79218e8601247
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-12 02:12:38 -0300

    Correção do diretório correto

--- backup-banco.sh
+++ backup-banco.sh
@@ -5,7 +5,7 @@ mkdir -p /backups-databases || exit 1
 DRT=/backups-databases/backup-$DATA.sql
 if [-z "$SENHA"]
 then
-   mysqldump -u USR --all-databases
+   mysqldump -u USR --all-databases > $DRT
 else
    mysqldump -u $USR --password=$SENHA --all-databases > $DRT
 fi
