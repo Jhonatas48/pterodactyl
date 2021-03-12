a6d7f8fc7b140c6a0abf207b749fd27afeb3c38b
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-12 01:59:21 -0300

    Variável

--- backup-banco.sh
+++ backup-banco.sh
@@ -3,4 +3,4 @@ SENHA=''
 DATA=$(date +'%d-%m-%Y')
 mkdir -p /backups-databases || exit 1
 DRT=/backups-databases/backup-$DATA.sql
-mysqldump -u USR --password=$SENHA --all-databases > $DRT
+mysqldump -u $USR --password=$SENHA --all-databases > $DRT
