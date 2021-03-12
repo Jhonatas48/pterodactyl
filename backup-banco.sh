d069a82d7a292f4265578a96fd213b650985df2e
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-12 02:09:38 -0300

    Update com uma condicional

--- backup-banco.sh
+++ backup-banco.sh
@@ -3,4 +3,9 @@ SENHA=''
 DATA=$(date +'%d-%m-%Y')
 mkdir -p /backups-databases || exit 1
 DRT=/backups-databases/backup-$DATA.sql
-mysqldump -u $USR --password=$SENHA --all-databases > $DRT
+if [-z "$SENHA"]
+then
+   mysqldump -u USR --all-databases
+else
+   mysqldump -u $USR --password=$SENHA --all-databases > $DRT
+fi
