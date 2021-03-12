084149fd92bb7fbae3b5ea73e24d29823721f734
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-03-12 11:49:01 -0300

    Update

--- teste-script.sh
+++ teste-script.sh
@@ -3,9 +3,9 @@ SENHA='testando'
 DATA=$(date +'%d-%m-%Y')
 mkdir -p /backups-databases || exit 1
 DRT=/backups-databases/backup-$DATA.sql
-if ["$SENHA" -eq ''];then
+if [-z "$SENHA"];then
    mysqldump -u USR --all-databases > $DRT
 else
-   echo $SENHA
+   
    mysqldump -u $USR --password=$SENHA --all-databases > $DRT
 fi
