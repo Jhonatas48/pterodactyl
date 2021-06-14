362e10e7832c6c6a8c63a18d798a16b75d152e7d
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-06-13 22:06:47 -0300

    Update backup-banco.sh

--- backup-banco.sh
+++ backup-banco.sh
@@ -5,7 +5,7 @@ mkdir -p /backups-databases || exit 1
 DRT=/backups-databases/backup-$DATA.sql
 if [[-z $SENHA] || [-n $SENHA]]
 then
-   mysqldump -u USR --all-databases > $DRT
+   mysqldump -u $USR --all-databases > $DRT
    mysqlcheck -u root --auto-repair --optimize --all-databases
 else
    mysqldump -u $USR --password=$SENHA --all-databases > $DRT
