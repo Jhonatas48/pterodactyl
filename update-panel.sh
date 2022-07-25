945c9a158154b8dcdc765f9f9a63e50056707e29
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2022-07-25 15:01:59 -0300

    criando o sistema de update

---
+++ update-panel.sh
@@ -0,0 +1,13 @@
+apt-get update && apt-get upgrade
+echo Script feito por Jhonatas!!
+cd /var/www/pterodactyl
+php artisan down
+curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+chmod -R 755 storage/* bootstrap/cache
+composer install --no-dev --optimize-autoloader
+php artisan view:clear
+php artisan config:clear
+php artisan migrate --seed --force
+chown -R www-data:www-data /var/www/pterodactyl/*
+php artisan queue:restart
+php artisan up
