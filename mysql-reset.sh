509949d9783e6f75a7e8fda07e4ec65f3242e84c
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2022-12-06 08:12:19 -0300

    Update mysql-reset.sh

--- mysql-reset.sh
+++ mysql-reset.sh
@@ -1,20 +1,12 @@
-output(){
- echo -e '\e[36m'$1'\e[0m';
-}
- 
-mariadb_root_reset(){
- service mysql stop
- mysqld_safe --skip-grant-tables >res 2>&1 &
- sleep 5
- rootpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
- Q1="use mysql;"
- Q2="UPDATE user SET password=PASSWORD('$rootpassword') WHERE user='root';"
- Q3="FLUSH PRIVILEGES;"
- SQL="${Q1}${Q2}${Q3}"
- mysql mysql -e "$SQL"
- pkill mysqld
- service mysql restart
- output "Your MariaDB root password is $rootpassword"
-}
- 
-mariadb_root_reset
+    SERVER_IP=$(dig +short myip.opendns.com @resolver1.opendns.com -4)
+    adminpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+    Q0="SET old_passwords=0;"
+    Q1="SET PASSWORD FOR 'admin'@'$SERVER_IP' = PASSWORD('$adminpassword');"
+    Q2="FLUSH PRIVILEGES;"
+    SQL="${Q0}${Q1}${Q2}"
+    mysql mysql -e "$SQL"
+    output "New database host information:"
+    output "Host: $SERVER_IP"
+    output "Port: 3306"
+    output "User: admin"
+    output "Password: $adminpassword"
