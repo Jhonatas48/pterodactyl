fd3e14019f71c0571a608f7bcdd56373db392237
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2023-07-17 17:03:20 -0300

    Add files via upload

---
+++ phpmyadmin.sh
@@ -0,0 +1,38 @@
+    output "Installing phpMyAdmin..."
+    apt update
+	apt upgrade
+	apt install curl -y
+	cd /var/www/pterodactyl/public
+    rm -rf phpmyadmin
+    wget https://files.phpmyadmin.net/phpMyAdmin/${PHPMYADMIN}/phpMyAdmin-${PHPMYADMIN}-all-languages.zip
+    unzip phpMyAdmin-${PHPMYADMIN}-all-languages.zip
+    mv phpMyAdmin-${PHPMYADMIN}-all-languages phpmyadmin
+    rm -rf phpMyAdmin-${PHPMYADMIN}-all-languages.zip
+    cd /var/www/pterodactyl/public/phpmyadmin
+
+    SERVER_IP=$(curl -s http://checkip.amazonaws.com)
+    BOWFISH=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 34 | head -n 1`
+    bash -c 'cat > /var/www/pterodactyl/public/phpmyadmin/config.inc.php' <<EOF
+<?php
+/* Servers configuration */
+\$i = 0;
+/* Server: MariaDB [1] */
+\$i++;
+\$cfg['Servers'][\$i]['verbose'] = 'MariaDB';
+\$cfg['Servers'][\$i]['host'] = '${SERVER_IP}';
+\$cfg['Servers'][\$i]['port'] = '';
+\$cfg['Servers'][\$i]['socket'] = '';
+\$cfg['Servers'][\$i]['auth_type'] = 'cookie';
+\$cfg['Servers'][\$i]['user'] = 'root';
+\$cfg['Servers'][\$i]['password'] = '';
+/* End of servers configuration */
+\$cfg['blowfish_secret'] = '${BOWFISH}';
+\$cfg['DefaultLang'] = 'en';
+\$cfg['ServerDefault'] = 1;
+\$cfg['UploadDir'] = '';
+\$cfg['SaveDir'] = '';
+\$cfg['CaptchaLoginPublicKey'] = '6LcJcjwUAAAAAO_Xqjrtj9wWufUpYRnK6BW8lnfn';
+\$cfg['CaptchaLoginPrivateKey'] = '6LcJcjwUAAAAALOcDJqAEYKTDhwELCkzUkNDQ0J5'
+?>    
+EOF
+ chown -R www-data:www-data * /var/www/pterodactyl
